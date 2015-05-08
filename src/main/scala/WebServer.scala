af0c6ac02c02de481d761f74a6618e42f1fed007
Bartek Tomala <bartholomaios@gmail.com>
2015-05-08 18:15:23 +0200

    remove depreciated trait Bootable

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -2,19 +2,16 @@ import akka.actor.ActorSystem
 import akka.stream.ActorFlowMaterializer
 import com.typesafe.config.ConfigFactory
 import http.HttpService
-import akka.kernel.Bootable
 
-object WebServer extends Bootable {
+object WebServer extends App {
 
   implicit val system = ActorSystem("akka-http-sample")
   implicit val materializer = ActorFlowMaterializer()
 
   val config = ConfigFactory.load
 
-  def startup = {
-    HttpService.run(config)(system, materializer)
-  }
+  HttpService.run(config)(system, materializer)
 
-  def shutdown = system.shutdown()
+//  def shutdown = system.shutdown()
 
 }
